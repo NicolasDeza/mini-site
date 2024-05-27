e56a975e87261ac34cdf7e40c732632d37a5b674
NicolasDeza <nicolasdeza@hotmail.be>
2024-05-27 21:13:34 +0200

    modif margin de la page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { renderFooter } from "/components/footer.js";
 import { renderNav } from "/components/nav.js";
+import "/reset.css";
 import "/style.css";
 
 const app = document.getElementById("app");
@@ -10,7 +11,7 @@ app.innerHTML = `
   <section>
   <h2>Innovons pour un avenir durable</h2>
   </section>
-  <section id="about">
+  <section>
     <h2>À Propos de Nous</h2>
     <p>EcoNexa Solutions se consacre à la création de technologies écologiques innovantes pour promouvoir un avenir durable. Nous croyons fermement que chaque petite action peut avoir un grand impact sur notre planète.</p>
   </section>
